1e015eff0df37d0fd22039e59c809b15444d8997
KarthikGowdaRamakrishna <ramakrishna.ka@northeastern.edu>
2024-04-10 15:49:54 -0400

    Ramp bugfixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { App } from "./App"
+import { AppContextProvider } from "./components/AppContextProvider"
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <AppContextProvider>
     <App />
-  </React.StrictMode>
-);
+  </AppContextProvider>
+)
